3d2b3e04aacbbfd70c898d64421047383b2263b7
Matt Dunphy <mdunphy@weblinc.com>
2015-07-22 19:00:01 -0400

    Code cleanup!

--- dr110.js
+++ dr110.js
@@ -3,7 +3,7 @@
 var clicking = false;
 var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
 var songs=new Array(2); // Consider going JSON?
-var Banks=new Array(2); 
+var Banks=new Array(2);
 var Sequences=new Array(7);
 var shiftEngaged = false;
 var tempo=120;
@@ -18,7 +18,7 @@ var bank = 1;
 var measure = 1;
 var step = 1; // Theoretically goes up to 16
 var song = 1;
-var patternLength = 16; 
+var patternLength = 16;
 var pattern = 1;
 var selectedMode = 4;
 var hihatAudio = new Audio(); // Global hihat audio for supporting open/closed hihat cutoff
@@ -27,7 +27,7 @@ var sharedDisplayInstrument = 0;
 
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-}
+};
 
 startBeat = function(){
 	clearInterval(DR110HEART);
@@ -36,14 +36,14 @@ startBeat = function(){
 	step = 1;
 	rate = 15000/tempo;
 	DR110HEART=setInterval("staticTempoBeat()",rate);
-}
+};
 
 stopBeat = function(){
 	clearInterval(DR110HEART);
 	clearTimeout(DR110HEART);
 	DR110HEART = "";
 	step = 1;
-}
+};
 
 staticTempoBeat = function(){
 	nextStep();
@@ -53,16 +53,16 @@ staticTempoBeat = function(){
 				// Don't play the open hihat if it lands on the same beat as a closed hihat.
 			}
 			else if((q == 4) && (Sequences[3][step] == 1)){
-				playFile(instruments[7],(Sequences[0][step]==1)); // Play the pedal hat 
+				playFile(instruments[7],(Sequences[0][step]==1)); // Play the pedal hat
 			}
 			else{
-				
+
 				playFile(instruments[q],(Sequences[0][step]==1));
 			}
-			
+
 		}
 	}
-}
+};
 
 mode = {
 	1:'Song Play',
@@ -70,7 +70,7 @@ mode = {
 	3:'Song Write',
 	4:'Step Write',
 	5:'Tap Write'
-}
+};
 
 instruments = {
 	0:'Accent',
@@ -81,7 +81,7 @@ instruments = {
 	5:'Cymbal',
 	6:'HandClap',
 	7:'PedalHat' //Only occurs when open and closed hats are triggered simultaneously
-}
+};
 
 initialize = function(){
 	$('#bank').html(String.fromCharCode(64+bank));
@@ -89,15 +89,15 @@ initialize = function(){
 	$('#song').html((song==1)?"I":"II");
 	$('#pattern').html(pattern);
 	updateSequenceDisplay();
-}
+};
 
 writeDot = function(thisInstrument,thisStep,override){
-	if(((thisInstrument == selectedInstrument) || (thisInstrument == 0 && selectedInstrument == override))&& thisStep == step){
+	if(((thisInstrument == selectedInstrument) || (thisInstrument === 0 && selectedInstrument == override))&& thisStep == step){
 		$("#sequ" + instruments[thisInstrument]).append("<img src=\"./images/dot_blink.png\" class=\"step" + thisStep + "\">");
 	}
 	else{
 		try{
-			if(Sequences[(override)?override:thisInstrument][thisStep] == 0){
+			if(Sequences[(override)?override:thisInstrument][thisStep] === 0){
 				$("#sequ" + instruments[thisInstrument]).append("<img src=\"./images/dot_off.png\" class=\"step" + thisStep + "\">");
 			}
 			else{
@@ -108,11 +108,11 @@ writeDot = function(thisInstrument,thisStep,override){
 			//Ha!
 		}
 	}
-}
+};
 
 updateSequenceDisplay = function(){
 	var seqHTML = "";
-	for(i=0;i<7;i++){ 
+	for(i=0;i<7;i++){
 		if(i > 0 && i < 5){
 			$("#sequ" + instruments[i]).html("");
 			for(j=1;j<17;j++){
@@ -123,13 +123,13 @@ updateSequenceDisplay = function(){
 			if(i == sharedDisplayInstrument){
 				switch(i){
 					case 5:
-						$("#splitDisplay").html("&nbsp;&nbsp;&nbsp;/CY/"); 				//hackity...
+						$("#splitDisplay").html("&nbsp;&nbsp;&nbsp;/CY/");	//hackity...
 					break;
 					case 6:
 						$("#splitDisplay").html("&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;/CP");	//hack...
 					break;
 					case 0:
-						$("#splitDisplay").html("AC/&nbsp;&nbsp;/"); 					//hack.
+						$("#splitDisplay").html("AC/&nbsp;&nbsp;/");				//hack.
 					break;
 				}
 				$("#sequ" + instruments[0]).html("");
@@ -139,11 +139,11 @@ updateSequenceDisplay = function(){
 			}
 		}
 	}
-}
+};
 
 handle_keydown = function(e){
 	var code;
-	if (!e) var e = window.event;
+	if (!e) e = window.event;
 	if (e.shiftKey==1){
         $("#butShift").trigger('click');
     }
@@ -200,14 +200,14 @@ handle_keydown = function(e){
 			$("#butChihat").trigger('click');
 		break;
 		case 'N':
-			$("#butCymbal").trigger('click');	
+			$("#butCymbal").trigger('click');
 		break;
 		case 'M':
 			$("#butHandClap").trigger('click');
 		break;
-	
+
 	}
-}
+};
 
 playFile = function(filename,withAccent){
 	if(filename==instruments[3] || filename==instruments[4] || filename==instruments[7]){ // Handle hihats as best as I can
@@ -233,7 +233,7 @@ playFile = function(filename,withAccent){
 		audio.volume=(withAccent)?volume+accent:volume;
 		audio.play();
 	}
-}
+};
 
 numberButton = function(number){
 	if(number==6){
@@ -246,7 +246,7 @@ numberButton = function(number){
 				else{
 					$("#patternLength").css('left','300px');
 				}
-				
+
 				Sequences[7] = patternLength;
 			}
 			else{
@@ -276,14 +276,14 @@ numberButton = function(number){
 			initialize();
 		});
 	}
-}
+};
 
 nextStep = function(){
 	step++;
 	if(step>patternLength){
 		step = 1;
 	}
-}
+};
 
 createKnob = function(knobType){
 	$("#" + knobType + "Control").mousedown( function( eventObj ) {
@@ -297,10 +297,11 @@ createKnob = function(knobType){
 		$("#" + knobType + "Control").css('top',$("#knob" + knobType.capitalize()).offset().top - 83 + "px");
 		$('#device').focus(); //Trying to fix knob drag issue.
 	});
-}
+};
+
 assignKnob = function(knobType,knobFunction){
 	$("#" + knobType + "Control").mousemove(function(eventObj){
-		if(clicking == false) return;
+		if(clicking === false) return;
 		var minValue = $("#" + knobType + "Control").offset().top;
 		var maxValue = $("#" + knobType + "Control").height() + $("#" + knobType + "Control").offset().top - minValue;
 		var currentValue = eventObj.pageY - minValue;
@@ -312,37 +313,39 @@ assignKnob = function(knobType,knobFunction){
 			$("#knob" + knobType.capitalize()).css('-webkit-transform','rotate(-' + rot +'deg)');
 		}
 	});
-}
+};
 
 tempoFunction = function(pct){
 	tempo = Math.round((1-pct/100) * maxTempo);
 	$("#tempo").text(tempo + " bpm" );
-}
+};
+
 volumeFunction = function(pct){
 	volume = 0.88 - (pct/100);
 	$("#volume").text(Math.round(volume * 100));
-}
+};
+
 accentFunction = function(pct){
-	accent = .22-(pct/100) * .22;
+	accent = .22-(pct/100) * 0.22;
 	$("#accent").text(Math.round(accent * 100));
-}
+};
 
 $(document).ready(function(){
 	document.onkeydown = handle_keydown;
 
 	$(document).mouseup(function(){
 		clicking = false;
-	})
-	
+	});
+
 	createKnob("tempo");
 	assignKnob("tempo","tempoFunction");
-	
+
 	createKnob("volume");
 	assignKnob("volume","volumeFunction");
-	
+
 	createKnob("accent");
 	assignKnob("accent","accentFunction");
-	
+
 	//Initialize instrument sequences
 	for(i=0;i<7;i++){
 		numberButton(i);
@@ -352,13 +355,13 @@ $(document).ready(function(){
 		}
 	}
 	Sequences[7] = 16;
-	
+
 	//Initialize banks with empty sequences. TODO: Load with original Boss factory presets instead. Heh.
 	for(i=1;i<5;i++){								//Bank
-		Banks[i] = new Array(8);					 
+		Banks[i] = new Array(8);
 		for(j=1;j<9;j++){							//Pattern
 			SequencesTmp = new Array(7);
-			for(i2=0;i2<7;i2++){					//Instrument 
+			for(i2=0;i2<7;i2++){					//Instrument
 				SequencesTmp[i2] = new Array(16);
 				for(j2=1;j2<17;j2++){				//Step
 					SequencesTmp[i2][j2] = 0;
@@ -368,10 +371,10 @@ $(document).ready(function(){
 			Banks[i][j] = SequencesTmp;
 		}
 	}
-	
+
 	//Write out sequencer dots.
 	initialize();
-	
+
 	{ // Grouping buttons 7,8
 		$('#but7').bind('click', function(event) {
 			if(shiftEngaged){	//Clear pattern
@@ -389,7 +392,7 @@ $(document).ready(function(){
 			}
 			initialize();
 		});
-		
+
 		$('#but8').bind('click', function(event) {
 			if(shiftEngaged){
 				//TODO:Reset Measure [SONG MODE]
@@ -402,12 +405,12 @@ $(document).ready(function(){
 	}
 
 	{ // Grouping shift, abcd, start, stop
-	
+
 		$('#butShift').bind('click', function(event) {
 			$('#butShift').toggleClass('engaged');
 			shiftEngaged=(shiftEngaged)?false:true;
 		});
-		
+
 		$('#butABCD').bind('click', function(event){
 			if(shiftEngaged){
 				song=(song==1)?2:1;
@@ -425,7 +428,7 @@ $(document).ready(function(){
 			}
 			initialize();
 		});
-	
+
 		$('#butStart').bind('click', function(event){
 			switch(selectedMode){
 				case 1:
@@ -448,7 +451,7 @@ $(document).ready(function(){
 					break;
 				}
 		});
-		
+
 		$('#butStop').bind('click', function(event){
 			switch(selectedMode){
 				case 1:
@@ -587,7 +590,8 @@ $(document).ready(function(){
 			}
 		});
 	}
-	
+
 	$("#knobTempo").css('-moz-transform','rotate(-232deg)');
 	$("#knobTempo").css('-webkit-transform','rotate(-232deg)');
 });
+
