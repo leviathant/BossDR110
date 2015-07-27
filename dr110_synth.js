10747eab0841132aea77b6c941ab8776852eb8f1
Matt Dunphy <mdunphy@weblinc.com>
2015-07-27 08:38:54 -0400

    Initial playback logic; Envelope timing information

--- dr110_synth.js
+++ dr110_synth.js
@@ -2,6 +2,35 @@ function Kick(context) {
   this.context = context;
 }
 
+/*
+HH OSC
+0.88ms
+1.16ms
+3.28ms
+2.25ms
+
+Alt:
+0.87ms
+1.22ms
+3.15ms
+2.15ms
+
+Envelope Times:
+TestPoint   Time  Voltage   Voice(?)
+5 700ms   6v    (OH)
+6 80ms    6v    (CH)
+7 60ms    6v    (CY)
+8 900ms   6v    (CY)
+9 1.4s    2.7v  (CY)
+11 140ms   5v   (HC)
+12 700ms   5v   (HC)
+13 100ms   5.7v (SD)
+14 120ms   5.7v (AC)
+
+Handclap Retrigger Time:
+10 10ms x3
+*/
+
 Kick.prototype.setup = function() {
   this.osc = this.context.createOscillator();
   this.gain = this.context.createGain();
@@ -34,7 +63,7 @@ var sixteenths = [
   "0:1", "0:1:1","0:1:2","0:1:3",
   "0:2", "0:2:1","0:2:2","0:2:3",
   "0:3", "0:3:1","0:3:2","0:3:3"
-  ]
+  ];
 
 var Score = {};
 
@@ -42,14 +71,41 @@ var sequence_to_tone = function(seq) {
   var kick  = new Kick(context);
   Tone.Transport.bpm.value = tempo;
 
-  Score.Kick = [];
+  for(circuit=0; circuit<=circuits.length; circuit++){
+    Score[circuits[circuit]]=[];
+  }
+  Score["PH"] = [];
+
   for(i=1; i<=seq.pattern_length; i++){
     if(seq.BD[i]==1){
-     Score.Kick.push(sixteenths[i-1]);
+     Score.BD.push(sixteenths[i-1]);
+    }
+    if(seq.SD[i]==1){
+     Score.SD.push(sixteenths[i-1]);
+    }
+    if(seq.CH[i]==1){
+      if(seq.OH[i]==1){                 // Pedal the hat
+        Score.PH.push(sixteenths[i-1]);
+      }
+      else{
+        Score.CH.push(sixteenths[i-1]);
+      }
+    }
+    if(seq.OH[i]==1 && seq.CH[i]===0){  //Only open if not also closed.
+     Score.OH.push(sixteenths[i-1]);
+    }
+    if(seq.HC[i]==1){
+     Score.HC.push(sixteenths[i-1]);
+    }
+    if(seq.CY[i]==1){
+     Score.CY.push(sixteenths[i-1]);
+    }
+    if(seq.BD[i]==1){
+     Score.BD.push(sixteenths[i-1]);
     }
   }
 
-  Tone.Note.route("Kick", function(time){
+  Tone.Note.route("BD", function(time){
     kick.trigger(time);
   });
 
