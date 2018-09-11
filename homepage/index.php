c735adaeec34e30064f98f8586c8993d71e3f4fd
Matt Dunphy <m.dunphy+git@gmail.com>
2018-09-10 23:43:22 -0400

    Adding my old homepage into the repo, for kicks.

---
+++ homepage/index.php
@@ -0,0 +1,464 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+		<title>
+			The Boss DR-110 Dr Rhythm Graphic Information Homepage
+		</title>
+		<style>
+			body{
+				background-color:#333;
+				color:#EEE;
+				font-family:arial, helvetica, sans-serif;
+				margin:0;
+			}
+			#intro{
+			background-image:url('bg.png');
+			background-repeat:no-repeat;
+			background-position:center;
+			min-height:475px;
+			}
+			#header{
+				text-align:center;
+			}
+			.leftBox{
+				float:left;
+				width:48%;
+			}
+			.rightBox{
+				float:right;
+				width:48%;
+			}
+			#nav li{
+				display: inline;
+				padding-right:10px;
+			}
+			#nav li a{
+			padding-top: 10px;
+			padding-bottom: 10px;
+			}
+			#nav {
+			padding-top: 10px;
+			padding-bottom: 10px;
+			background-color:#555;
+			}
+			a, a:visited, a:active{
+				color:#eee;
+			}
+			#content{
+				margin:10px;
+			}
+			h2{
+			margin-left:10px;
+			}
+			h3{
+			margin-left:15px;
+			}
+			p{
+			margin-left:20px;
+			}
+			
+			.ebayListing{
+				overflow:auto;
+				width:32%;
+				margin-right:1%;
+				float:left;
+			}
+			.eBayItem{
+					height: 160px;
+					border: 1px solid black;
+					border-top: 0;
+					padding-top: 5px;
+					padding-bottom: 5px;
+					background-color: #333;
+					
+				}
+				
+				.itemTitle{
+					padding: 3px;
+					border: 1px solid black;
+					border-bottom: 0;
+					font-size: 10pt;
+					font-weight: bold;
+					height: 70px;
+					background-color:#222;
+				}
+				.itemDate{
+					font-size: 7pt;
+				}
+				.ebayImage{
+					float: left;
+					border: 0;
+					padding: 5px;
+				}
+				
+				.calloutImage{
+					margin-left: auto;
+					margin-right: auto;
+					border: 0;
+					padding: 5px;
+				}
+				
+				
+			.ebayItemActions{
+					margin-bottom: 10px;
+				}
+				
+				.ebayItemActions ul{
+					list-style: none;
+					margin: 0;
+					padding: 0;
+				}
+				
+				.ebayItemActions ul li{
+					display: inline;
+				}
+				.ebayItemActions ul:first-child{
+				margin-left: 3px;
+				}
+				
+				.ebayItemActions ul li a{
+					display: block;
+					height: 30px;
+					width: 90px;
+					float: left;
+					margin-bottom: 10px;
+				}
+				
+				.linkBid, .linkWatch, .linkBuy{
+					text-indent: -235157px;
+				}
+				.linkBid{
+					background-image:url('/features/merch/b_bid.png');
+				}
+				.linkBuy{
+					background-image:url('/features/merch/b_buy.png');
+				}
+				.linkWatch{
+					background-image:url('/features/merch/b_watch.png');
+				}
+			
+		</style>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-763021-5']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+		<script type="text/javascript">
+		/* <![CDATA[ */		
+	
+		<!--
+		function toggleContent(divID)
+		{
+		
+			$('.pagecontent').fadeOut(400);
+			$('#' + divID).delay(500).fadeIn(); 
+			_gaq.push(['_trackPageview','/' + divID]);
+			//pageTracker._trackPageview('/' + divID);
+		}
+		$(document).ready(function(){
+		$('.pagecontent').hide();
+		toggleContent('intro');
+		});
+		//-->
+		/* ]]> */
+		</script>
+		
+	</head>
+	<body>
+		<div id="header">
+			<h1>The Boss DR-110 Dr Rhythm Graphic Information Homepage</h1>
+			<img src="buttonsprites.png" id="headerImage"/>
+			<ul id="nav">
+				<li><a href="javascript:toggleContent('links');">Mods</a></li>
+				<li><a href="javascript:toggleContent('schematics');">Schematics</a></li>
+				<li><a href="javascript:toggleContent('specs');">Specifications</a></li>
+				<li><a href="javascript:toggleContent('instructions');">Programming Instructions</a></li>
+				<li><a href="javascript:toggleContent('trivia');">Trivia</a></li>
+				<li><a href="javascript:toggleContent('audio');">Audio</a></li>
+				<li><a href="javascript:toggleContent('auctions');">Buy a DR-110</a></li>
+			</ul>
+		</div>
+		
+		<div id="content">
+		
+			<div id="intro" class="pagecontent" style="display: block;">
+				<p>
+					This page came into existence because I bought a DR-110 with schematics in 1999 and had the desire to modify Roland's final analog drum machine, but didn't have the know-how. 
+					In the years since, you can find those schematic scans all around the web, and many people have posted modification ideas, and many more have modified their DR-110s.
+					</p>
+				<p>
+					Before posting those schematics, it wasn't unusual to see Boss DR-110s sell for $50 and under. For better or for worse, those days are long gone. Consider this page a really big supplement to <a href="Boss-dr110-manual.pdf">the manual</a> - there's really more than you'd ever want to know about the DR-110 here.
+				</p>
+			</div>
+			
+			<div id="trivia" class="pagecontent">
+				<img src="110-stuff.jpg" alt="A Boss DR-110 with all the accessories." style="float:right; margin-left:20px;"/>
+				
+				<ul>
+					<li><em>Release Date</em>: Sometime in 1983. There were several revisions of the manual - The back of the first manual I had said "DR-110 OWNER'S MANUAL '84 SEP. A-4" and I have another that's marked "'83 NOV. B-4". <a href="Boss-dr110-manual.pdf">A PDF version of the Boss DR-110 manual</a> is labeled "'86 MAR. C-3"</li>
+
+					<li><em>Number Manufactured</em>: Well, at least 5000, going by my serial number.</li>
+					<li><em>Synthesis Type</em>: 100% Pure Analogue</li>
+					<li><em>Original 1983 Price</em>: $200</li>
+					<li><em>Fair Second Hand Price</em>: $100-$200</li>
+					<li><em>Included with original purchase</em>: Silver vinyl carrying case, 2 silver boss stickers, 55 page manual</li>
+
+					<li><em>Optional Adapters</em>: Boss PSA-120, 220 or 240</li>
+					<li>At the time, Roland produced a 40 page booklet (written by Sandy Feldstein) for the DR-110, "A Step-By-Step Approach to learning the Dr. Rhythm Graphic" (As if the manual wasn't simple enough). "This Handy Guide will teach you how to:
+					<ul>
+					        <li>Write Rock Beats</li>
+
+					        <li>Write Rock Fills</li>
+					        <li>Write a Rhythm Chart for a 12-Bar Blues</li>
+					        <li>Write a Rhythm Chart for a 32-Bar Tune</li>
+					        <li>Create Your Own Rhythms and Charts"</li>
+					</ul>
+				    It is pretty cheesy, and was sold for $2.95 retail by Alfred Publishing Co., Inc. (Book #2453) This was also published in 1984.</li>
+
+				  <li><em>Test Mode</em>: When you turn on your DR-110, hold down the start and stop buttons. The entire LED screen should show, though dimly. Press all your blue buttons, and if they all work, a big OK should appear.</li>
+			  </ul>
+			</div>
+			
+			<div id="schematics" class="pagecontent">
+				<h1>DR-110 Schematics</h1>
+
+				<a href="110scheme.zip">Click here to download the complete original DR-110 Schematics</a>
+				<p>SCHEMATICS ARE GIF IMAGES in ZIP FORMAT. They have been photographed and scanned directly from the official Roland service manual.</p>
+				<p>Please, if you download these schematics and perform even a modest modification, at least <a href="mailto:leviathant@gmail.com">let me know</a>.  This zip includes the schematics, waveform diagrams, the parts list, disassembly instructions.  Download, modify, spread the word. </p>
+			</div>  
+			
+			<div id="links" class="pagecontent">
+				<h2>Boss DR-110 Links</h2>
+				<div style="overflow:auto;">
+					<div class="leftBox">
+						<h3><a href="http://www.sdiy.org/richardc64/new_drums/dr110/dr110a1.html">An analysis of the DR-110 Cymbal, Hi-Hat and Clap,
+		with suggested modifications and proposed clones</a></h3>
+						<p>The title pretty much nails it.</p>
+						<h3><a href="http://www.vidiotsquad.com/synth-stuff/dr-110-mods/">Vidiot Squad DR-110 mods</a></h3>
+						<p>Strips down the Resonant Frequency and "Ultimate Analog" mods to very simple instructions, and goes into some detail into making his DR-110 MIDI-triggerable</p>
+						<h3><a href="./resFreq/">Resonant Frequency: DR-110 Mods</a></h3>
+						<p>Add further control to your bass drum pitch, snare impact, OHH decay, Clap pitch, plus trigger output &amp; more. Text and schematics included.</p>
+					</div>
+					<div class="rightBox">				
+						<h3><a href="110mods.html">The Ultimate Analogue Percussion Machine</a></h3>
+						<p>A fellow named Rob sent this to the Analogue Heaven mailing list years ago, and it includes fifteen different adjustments to the DR-110's sound generating circuits.</p>
+						<h3><a href="http://en.wikipedia.org/wiki/Boss_Doctor_Rhythm_DR-110">The Wikipedia entry for the Boss DR-110</a></h3>
+						<h3><a href="http://www.bitrotten.com/dr110/">Virtual DR-110</a></h3>
+						<p>I finally (mostly) made a virtual Boss DR-110 - not in Flash, like I'd planned in 1999, but in Javascript! A thought that would have made me ill in 1999.</p>
+						<h3><a href="http://www.circuitbenders.co.uk/forsale/DR110sync.html">Boss DR-110 DIN sync kit</a></h3>
+						<p> Slave your DR-110 to DIN Sync</p>
+					</div>
+				</div>
+				
+				<p>And a list of mods that <a href="http://electro-music.com/forum/post-348925.html#348925">Dan Lavin posted on electro-music.com</a>:</p>
+				<ul>
+					<li>replace C50 with a 4.7uF cap (adds more reverb to the clap)</li>
+					<li>replace C52 with a 4.7uF cap (adds more decay to the snare....more 80's sounding)</li>
+					<li>replace R70 with a 1K pot (kick pitch)</li>
+					<li>replace r86 with a 50K pot (kick decay)</li>
+					<li>added a switch with a 4.7uF cap across the kick pitch pot (bass boost)</li>
+					<li>replaced R81 with a 100K pot (snap level)</li>
+					<li>replace R80 with a 50K pot (snap tone)</li>
+					<li>paralleled R75 with a 50K pot (snare mix)</li>
+					<li>replace R76 with a 50K pot (snare impact pitch)</li>
+					<li>replaced R84 with a 50K pot (snare decay)</li>
+					<li>replaced R12 with a 1K pot (hhat filter)</li>
+					<li>replaced R10 with a 100K pot (hhat pitch)</li>
+					<li>replaced R22 with a 50K pot (hhat mix)</li>
+					<li>replaced R53 with a 50K pot (hhat closed decay)</li>
+					<li>replaced R20 with a 1M pot (hhat open decay)</li>
+					<li>replaced R16 with a 1K pot (cymbal filter)</li>
+					<li>replaced R44 with a 1M pot in series with a 1K fixed resistor (cymbal decay)</li>
+					<li>replaced R114 with a 50K pot (clap pitch)</li>
+					<li>replaced R120 with a 50K pot (clap filter)</li>
+					<li>replaced R113 with a 100K pot (clap R113)</li>
+				</ul>
+			</div>
+			
+			<div id="specs" class="pagecontent">
+				<h2>Spec Sheet</h2>
+				<h3>Transcribed from the Boss DR-110 Manual</h3>
+
+				<ul><li>Memory Capacity</li>
+				<li>
+					<ul>
+					      <li>32 kinds (8 x 4 Banks)</li>
+					      <li>Preset Rhythms 16</li>
+					      <li>Free for users 16</li>
+					</ul>
+
+				</li>
+				<li>Step Number</li>
+				<li>
+					<ul>
+					      <li>1 to 16 steps/1 to 12 steps</li>
+					</ul>
+				</li>
+				<li>Song</li>
+
+				<li>
+					<ul>
+					      <li>128 measures x 2 (I, II)<br />
+					      (Maximum of continuous measures 128) </li>
+					</ul>
+				</li>
+				<li>Sound Sources</li>
+
+				<li>
+					<ul>
+					      <li>Bass Drum</li>
+					      <li>Snare Drum</li>
+					      <li>Cymbal</li>
+					      <li>C. Hihat</li>
+					      <li>O. Hihat</li>
+
+					      <li>Hand Clap</li>
+					      <li>Accent</li>
+					</ul>
+				</li>
+				<li>Liquid Crystal Display x 1</li>
+
+				<li>Keys &amp; Controls</li>
+
+				<li>
+					<ul>
+					      <li>SHIFT key</li>
+					      <li>BANK Selector Key (A to D)</li>
+					      <li>Rhythm Selector Keys (1 to 8)</li>
+					      <li>START key</li>
+					      <li>STOP key</li>
+
+					      <li>Instrument Keys (Accent, Bass Drum, Snare Drum, O. Hihat, C. Hihat, Cymbal and Hand Clap)</li>
+					      <li>TEMPO Knob (45 to 300)</li>
+					      <li>BALANCE Knob</li>
+					      <li>ACCENT Knob</li>
+					      <li>VOLUME Knob</li>
+					</ul>
+
+				</li>
+				<li>Power Switch x 1</li>
+				<li>Jacks</li>
+				<li>
+					<ul>
+					      <li>DC 9V x 1</li>
+					            <li><ul><li>AC Adaptor Jack <br />(Optional AC Adaptor BOSS PSA-120, 220, or 240)</li></ul></li>
+
+					      <li>OUTPUT (P-BUS)</li>
+					            <li><ul><li>Standard Phone Jack</li>
+					            <li>Input/Output Impedance 10k</li></ul></li>
+					      <li>Headphones x 1</li>
+								<li>
+					            <ul><li>Stereo Mini Jack</li>
+
+					            <li>+6V, 10ms pulse</li></ul></li>
+					      <li>Power</li>
+					            <li><ul><li>6V (UM-3x4)</li>
+					            <li>(AC Adaptor: 9V)</li></ul></li>
+					      <li>Consumption</li>
+					            <li><ul><li>7mA (Min.) to 12 mA (Max.)</li></ul></li>
+
+					      <li>Dimensions</li>
+					            <li><ul><li>190 (W) x 110 (D) x 30 (H) mm</li>
+					            <li>7-1/2(W) x 4-5/16(D) x 1-3/16(H) in</li></ul></li>
+					      <li>Weight</li>
+					            <li><ul><li>450g/1 lb (including batteries)</li></ul></li>
+					      <li>Accessories</li>
+
+					            <li><ul><li>Dry Cell Batteries UM-3 (BR-3) x 4</li>
+					            <li>Case x 1</li></ul></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			
+			<div id="instructions" class="pagecontent">
+				<h2>PREPARING TO WRITE A RHYTHM</h2>
+
+				<h3>(Pattern Programming)</h3>
+				<ol>
+					<li> Connect an amplifier or headphones to the output of your DR-110.</li>
+					<li>	Turn the POWER on and set the VOLUME to a little under half.</li>
+
+					<li>Turn TEMPO, BALANCE, and ACCENT to their mid-points. In the beginning, you may wish to set the TEMPO slower. The second line is a good place to start.</li>
+					<li>Hold SHIFT button down and press button 4. STEP WRITE will appear in the lower right hand box of the LCD screen. This tells the DR-110 you are going to write a rhythm.</li>
+					<li>Press BANK SELECTOR button (A|B|C|D) to A. A will appear under BANK (lower middle box of screen). If B appears, press button again for A.</li>
+					<li>Press button 1. 1 will appear on the screen under RHYTHM. This tells the DR-110 you are going to store the beat in SELECTOR 1.</li>
+
+					<li>Hold SHIFT button down and press 7 (under PATTERN CLEAR). This will erase anything previously stored in SELECTOR 1.</li>
+					<li>Hold SHIFT button down and press 6 (12/16). You will see a little arrow at the top of the graph on your screen. The arrow will alternate between 12 and 16. Press 6 until the arrow is above 16. This tells the memory that you are writting a pattern with 4 beats in each measure and that each beat is divided in 4 parts for a total of 16. Since the procedure for preparing the DR. RHYTHM is always the same, the sooner you memorize this procedure, the more fun you'll have.</li>
+				</ol>
+				<h2>WRITING THE BEAT</h2>
+				<p>Select the instrument that you are going to enter by pressing the corresponding button. For example, the Closed Hi-Hat. Press the C. HIHAT button and on the screen you will see a flashing black dot on the C. HIHAT line. (note: Dot flashes at tempo machine is set at)</p>
+				<p>When you press the Start Button, a solid dot will appear. When you press the STOP key, the flashing dot will move to the next position. Each dot entered will indicate a note. </p>
+
+				 <h2>WRITING A SONG</h2>
+				 <ol>
+					<li> Make sure that Rhythm is not playing.</li>
+					<li> While holding the SHIFT Key down, press the button numbered 3. (Now the DR-110 is in the SONG WRITE mode.)</li>
+
+					<li>Select either Song number I or II. (While holding the SHIFT Key down, press the BANK Selector Burron, and the number changes I and II alternately each time it is pressed)</li>
+					<li>While holding the SHIFT Key down, press the button numbered 8. (Now the first measure of the Song is ready to start to play.)</li>
+					<li>Press the START Key. (Rhythm will start playing)</li>
+					<li>Select the Rhythm Pattern you wish to write into the first measure (The selected Rhythm Pattern is now shown in the Display and plays)</li>
+					<li>Press the ENTER key (O HIHAT). (Now the selected Rhythm Pattern is written into the first measure, and it will move to the second measure)</li>
+					<li>Select the Rhythm Pattern to be written into the second measure.</li>
+
+					<li>Press the ENTER Key. (The selected Rhythm Pattern is written into the second measure and it moves to the third measure.)</li>
+					<li>
+						<ul>
+						    <li> Likewise, keep writing up to the measures just before the final one.</li>
+						    <li>If you are to write the same Rhythm Pattern as the one before, just pressing the ENTER key will be sufficient.</li>
+						</ul>
+					</li>
+
+					<li>Select the Rhythm Pattern for the last measure.</li>
+					<li>Press the D.C. (C. HIHAT) (Now D.C. [da capo] is set to the final measure.)</li>
+					<li>Press the ENTER key. (The selected Rhythm Pattern is now written into the final measure, and it moves to the next measure.)</li>
+					<li>Press the STOP Key. Now try playing the Rhythm. </li>
+				</ol>
+			</div>
+			
+			<div id="audio" class="pagecontent">
+
+				<h3>Samples and Audio</h3>
+				<a href="http://www.bitrotten.com/dr110">Javascript DR-110 emulator</a>
+				This is where you should be able to download samples from the Boss DR-110, and samples of the DR-110 in action.  Until I get those samples, however, this text is all that is on this page.  The samples will likely be sourced from the same place everyone else gets them, the archives at Hyperreal.org.
+			</div>
+		
+			<div id="auctions" class="pagecontent">
+			<? 
+			require_once('../common/php/ebayFindItem_class.php');
+			$promoQuery = new ebay();
+			$promoQuery->query="DR-110 -(sample, user)";
+			$promoQuery->campaignID = "5336543519";
+			$promoQuery->categoryList = "38069";
+			$promoQuery->maxToDisplay = 10;
+			$promoQuery->sortOrder = "StartTimeNewest";
+			$promoQuery->siteLocation = "FrontPage";
+			$promoQuery->appid = "Bitrotte-1e4f-46a8-832c-1ac6e689b44e";
+			$promoQuery->search();
+			$searchResults = $promoQuery->results;
+			if (strlen($searchResults)){
+				echo $searchResults;
+			}
+			else{
+				echo "There are no DR-110s for sale on eBay at the moment.";
+			}
+			
+			?>
+			
+			</div>
+		</div>
+	</body>
+</html>
